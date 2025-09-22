dc7a7523d887da573e3781b4b35893314a7c702c
Marco <marco.palummieri@gmail.com>
2025-09-23 01:42:19 +0200

    feat: add hero section with images and styles
    - Created hero-section component with HTML structure including two images.
    - Added SCSS styles for the hero section to ensure proper layout and image display.
    - Implemented TypeScript component file for the hero section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
